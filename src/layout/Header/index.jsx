7607d6e02554b4cd68ff27ec11cdefa71feff806
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-10 23:06:04 +0000

    Style navbar, fix external links and add Github logo

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,23 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { BackButton, Search } from "../../components";
+import { BackButton } from "../../components";
+import logo from "../../logo.png";
 import "./style.css";
 
 export const Header = () => {
-    const [search, setSearch] = useState("");
-
-    const getInput = (search) => {
-        setSearch(search)
-    }
-
     return (
         <nav>
-            <Search getInput={getInput} />
-            <Link to={{ pathname: "https://github.com/pulls" }} target="_blank">Pull Requests</Link>
-            <Link to={{ pathname: "https://github.com/issues" }} target="_blank">Issues</Link>
-            <Link to={{ pathname: "https://github.com/marketplace" }} target="_blank">Marketplace</Link>
-            <Link to={{ pathname: "https://github.com/explore" }} target="_blank">Explore</Link>
-            <BackButton />
+            <img src={logo} alt="Github logo" />
+            <div id="right">
+                <Link to={{ pathname: "https://github.com/pulls" }} target="_blank">Pull Requests</Link>
+                <Link to={{ pathname: "https://github.com/issues" }} target="_blank">Issues</Link>
+                <Link to={{ pathname: "https://github.com/marketplace" }} target="_blank">Marketplace</Link>
+                <Link to={{ pathname: "https://github.com/explore" }} target="_blank">Explore</Link>
+                <BackButton />
+            </div>
         </nav>
     )
 }
