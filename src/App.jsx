b957d8e96cac35bb48ce8f6738c4adf82cd95c89
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-10 23:06:49 +0000

    Edit React app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
 import { Header, Footer } from "./layout";
 import { GitHubPage } from "./pages";
 
@@ -7,14 +6,7 @@ function App() {
   return (
     <>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <GitHubPage />
-        </Route>
-        <Route>
-          <h3>Path does not exist</h3>
-        </Route>
-      </Switch>
+      <GitHubPage />
       <Footer />
     </>
   );
