4c6dad3654e7076fef84e814035d827572ce04b4
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-10 11:03:25 +0000

    Update Form component

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import "./style.css";
+
+export const Form = ({ getInput }) => {
+    const [username, setUsername] = useState("");
+    const [formAlert, setFormAlert] = useState("Enter a username");
+
+    const reset = () => {
+        setUsername("");
+        setFormAlert("Enter a username");
+    };
+
+    const handleInput = (e) => {
+        console.log(e.target.value)
+        if (e.target.value) {
+            setUsername(e.target.value);
+            setFormAlert("");
+        } else {
+            setUsername("");
+            setFormAlert("Please enter a username");
+        }
+
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getInput(username);
+        setUsername("");
+        setFormAlert("");
+    };
+
+    return (
+        <form aria-label="form" onSubmit={handleSubmit, reset}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" value={username} onChange={handleInput} />
+            <input type="submit" disabled={formAlert} value={formAlert ? formAlert : "Search"} />
+        </form>
+    )
+}
--- src/pages/GitHubPage/index.jsx
+++ src/pages/GitHubPage/index.jsx
@@ -1,2 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, RepoInfo } from "../../components";
+
+export const GitHubPage = () => {
+    const [search, setSearch] = useState("");
+
+    const getInput = (input) => {
+        setSearch(input)
+    }
+
+    return (
+        <Form getInput={getInput} />
+    )
+}
