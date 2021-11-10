054785fb8e72137cec647e9a102e16d5b4ee6de3
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-10 12:31:30 +0000

    Add a footer

---
+++ src/layout/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const Footer = () => {
+    return (
+        <span id="footer">
+            <em>&copy; Forum Desai</em>
+        </span>
+    )
+}
