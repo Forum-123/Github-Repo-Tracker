fbe76ebaf6d7b991052f125901eae5f37baf1f66
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-11 09:20:01 +0000

    Call API for required info on each repo and add styling

--- src/components/RepoInfo/index.jsx
+++ src/components/RepoInfo/index.jsx
@@ -1,9 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Icon } from "@iconify/react";
 import "./style.css";
 
-export const RepoInfo = () => {
+export const RepoInfo = ({ name, description, visibility, language, url, stargazers, forks, issues, updated }) => {
+    const [showInfo, setShowInfo] = useState(false);
+
+    const show = () => {
+        setShowInfo((state) => !state);
+    };
+
+    const buttonText = () => {
+        if (showInfo) {
+            return "Hide Statistics"
+        } else {
+            return "Show Statistics"
+        }
+    }
+
     return (
-        <h5>Repo Information</h5>
+        <section className="repoDetails">
+            <a id="repoURL" href={url} target="_blank">{name}</a>
+            <span className="visibility">{visibility}</span>
+            <div id="right">
+                <button className="statsBtn" onClick={show}>{buttonText()}</button>
+            </div>
+            <p className="description">{description}</p>
+            {showInfo && <>
+                <p id="left">{language}</p>
+                <p id="right">Last updated: {updated} day(s) ago</p>
+                <p id="stats"><Icon icon="akar-icons:star" /> {stargazers} <Icon icon="eos-icons:fork-outlined" /> {forks} <Icon icon="octicon:issue-opened-24" /> {issues}</p>
+            </>}
+        </section>
     )
 }
