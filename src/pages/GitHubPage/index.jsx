9096ed0bfa2c801859966c45853813037b5ee456
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-11 09:31:13 +0000

    Edit landing page and render only Search component

--- src/pages/GitHubPage/index.jsx
+++ src/pages/GitHubPage/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Search } from "../../components";
+import "./style.css";
 
 export const GitHubPage = () => {
     return (
-        <>
-            <h4>Github page</h4>
-        </>
+        <section id="main">
+            <p>Search for a Github username and view their public repositories and their stats.</p>
+            <Search />
+        </section>
     )
 }
